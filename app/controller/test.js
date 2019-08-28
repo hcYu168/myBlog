6dffe4fa2c48f2c81c539fba5c48e2488afd4fa2
yhc <731636814@qq.com>
2019-08-28 11:39:49 +0800

    test文件添加222222
    test文件添加333333
    
    test文件添加4444444

--- app/controller/test.js
+++ app/controller/test.js
@@ -5,6 +5,9 @@ const Controller = require('egg').Controller;
 class TestController extends Controller {
   async echo() {
     console.log('dasdsdadsdssaddasas');
+    console.log('222222222');
+    console.log('333333333');
+    console.log('444444444');
   }
 }
 
